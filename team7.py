16b6638e0ae4a2fd43081d4bf53d27791b714670
spicyserpent <leuschnercarlene@gmail.com>
2020-02-18 23:21:50 +0000

    Added strategy to team7.py

--- team7.py
+++ team7.py
@@ -5,26 +5,26 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
-
-team_name = 'Turtles' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
     
+  
+#example1.py on testing-branch
+team_name = 'Turtle1'
+strategy_name = 'unexpected betayal'
+strategy_description = 'Betray at certain intervals'
+   
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    '''Make my move based on the history with this player.
     
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty. 
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
+    Returns 'c' or 'b' for collude or betray. '''
+   
+    if len(my_history) != 1:    
+      return "b"
+    elif len(my_history)%3 == int:    
+      return "b"
+    else:
+      return "c"
